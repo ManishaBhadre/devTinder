b5042bef1d16f08b786d63dcd2849014cdaef62b
ChetanMadde <69982409+ChetanMadde@users.noreply.github.com>
2026-06-05 13:32:19 +0530

    created express server

---
+++ src/app.js
@@ -0,0 +1,21 @@
+//console.log("Hello World from app");
+
+const express = require("express");
+
+const app = express();
+
+app.use("/test", (req, res) => {
+  res.send("Hello from test page route");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello hello hello!!!");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello from server using express testing nodemon!");
+});
+
+app.listen(7777, () => {
+  console.log("Server is running fine on port 7777");
+});
