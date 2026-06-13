6c4e2193bd109993843504c9df638a8a4456df50
ManishaBhadre <manisha.bhadre123@gmail.com>
2026-06-14 02:00:10 +0530

    Api level validations

--- src/app.js
+++ src/app.js
@@ -105,12 +105,27 @@ app.delete("/user", async (req, res) => {
 
 /**Update the data of the User by id */
 
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  //const userId = req.body.userId;
+
+  const userId = req.params?.userId;
 
   const data = req.body;
 
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k),
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Updates not allowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
     const user = await User.findByIdAndUpdate(userId, data, {
       returnDocument: "before",
       runValidators: true,
